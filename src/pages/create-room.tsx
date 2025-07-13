ed59c20ab4071f4351d374c01beb40948cc20c25
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-07-13 14:54:35 -0400

    Listando Salas Front

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,9 +1,12 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 
 type GetRoomsApiResponse = Array<{
   id: string;
   name: string;
+  questionsCount: number;
+  craetedAt: string; 
 }>
 
 export function CreateRoom(){
@@ -18,17 +21,34 @@ export function CreateRoom(){
   })
 
   return(
-    <div>
-      {isLoading && <p>Carregandi...</p>}
-      <div className="flex flex-col gap-1">
-        {data && 
-        data?.map(room =>{
-          return (
-           <Link key={room.id} to={`/room/${room.id}`}> 
-              {room.name} 
-           </Link>
-          )
-        })}
+    <div className="min-h-screen py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+           <div />
+
+           <Card>
+             <CardHeader>
+              <CardTitle>
+                Salas Recentes
+              </CardTitle>
+              <CardDescription>
+                Acesso rápido para as sala criadas recentemente
+              </CardDescription>
+             </CardHeader>
+             <CardContent className="flex flex-col gap-3">
+              {data?.map((room) =>{
+                return <div 
+                key={room.id} 
+                className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent"
+                >
+                  <div>
+                    <h3 className="font-medium">{room.name}</h3>
+                  </div>
+                </div>
+              })}
+             </CardContent>
+           </Card>
+        </div>
       </div>
     </div>
   )
